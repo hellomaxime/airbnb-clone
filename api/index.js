232994e050a31aeef00d855d1cf417f7984313ae
hellomaxime <shan.maxime@gmail.com>
2023-04-07 10:00:23 +0200

    wip main page

--- api/index.js
+++ api/index.js
@@ -108,21 +108,21 @@ app.post('/places', (req, res) => {
     const {
         title, address, addedPhotos,
         description, perks, extraInfo,
-        checkIn, checkOut, maxGuests,
+        checkIn, checkOut, maxGuests, price,
     } = req.body;
     jwt.verify(token, jwtSecret, {}, async (err, userData) => {
         if (err) throw err;
         const placeDoc = await Place.create({
-            owner: userData.id,
+            owner: userData.id, price,
             title, address, photos:addedPhotos,
             description, perks, extraInfo,
-            checkIn, checkOut, maxGuests,
+            checkIn, checkOut, maxGuests, price,
         });
         res.json(placeDoc);
     });
 })
 
-app.get('/places', (req, res) => {
+app.get('/user-places', (req, res) => {
     const {token} = req.cookies;
     jwt.verify(token, jwtSecret, {}, async (err, userData) => {
         const {id} = userData;
@@ -140,7 +140,7 @@ app.put('/places', async (req, res) => {
     const {
         id, title, address, addedPhotos,
         description, perks, extraInfo,
-        checkIn, checkOut, maxGuests,
+        checkIn, checkOut, maxGuests, price,
     } = req.body;
     jwt.verify(token, jwtSecret, {}, async (err, userData) => {
         const placeDoc = await Place.findById(id);
@@ -148,12 +148,16 @@ app.put('/places', async (req, res) => {
             placeDoc.set({
                 title, address, photos:addedPhotos,
                 description, perks, extraInfo,
-                checkIn, checkOut, maxGuests,
+                checkIn, checkOut, maxGuests, price,
             });
             await placeDoc.save();
             res.json('ok');
         }
     });
+});
+
+app.get('/places', async (req, res) => {
+    res.json(await Place.find());
 });
 
 app.listen(4000);
--- client/src/pages/PlacesFormPage.jsx
+++ client/src/pages/PlacesFormPage.jsx
@@ -17,6 +17,7 @@ export default function PlacesFormPage() {
     const [extraInfo, setExtraInfo] = useState('');
     const [checkIn, setCheckIn] = useState('');
     const [checkOut, setCheckOut] = useState('');
+    const [price, setPrice] = useState(100);
     const [maxGuests, setMaxGuests] = useState(1);
 
     const [redirect, setRedirect] = useState(false);
@@ -37,6 +38,7 @@ export default function PlacesFormPage() {
             setCheckIn(data.checkIn);
             setCheckOut(data.checkOut);
             setMaxGuests(data.maxGuests);
+            setPrice(data.price);
         })
     }, [id]);
 
@@ -66,7 +68,7 @@ export default function PlacesFormPage() {
         const placeData = {
             title, address, addedPhotos,
             description, perks, extraInfo,
-            checkIn, checkOut, maxGuests
+            checkIn, checkOut, maxGuests, price
         }
         if (id) {
             await axios.put('/places', {
@@ -101,7 +103,7 @@ export default function PlacesFormPage() {
                 {preInput('Extra info','house rules, etc')}
                 <textarea value={extraInfo} onChange={ev => setExtraInfo(ev.target.value)} />
                 {preInput('Check in&out times','add check in and out times, remember to have some time window for cleaning the room between guests')}
-                <div className="grid gap-2 sm:grid-cols-3">
+                <div className="grid gap-2 grid-cols-2 md:grid-cols-4">
                     <div>
                         <h3 className="mt-2 -mb-1">Check in time</h3>
                         <input type="text" value={checkIn} onChange={ev => setCheckIn(ev.target.value)} placeholder="14" />
@@ -113,6 +115,10 @@ export default function PlacesFormPage() {
                     <div>
                         <h3 className="mt-2 -mb-1">Max number of guests</h3>
                         <input type="number" value={maxGuests} onChange={ev => setMaxGuests(ev.target.value)} />
+                    </div>
+                    <div>
+                        <h3 className="mt-2 -mb-1">Price per night</h3>
+                        <input type="number" value={price} onChange={ev => setPrice(ev.target.value)} />
                     </div>
                 </div>
                 <div>
